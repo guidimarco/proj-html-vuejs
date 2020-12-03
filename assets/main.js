22b99eeb01de8a53fe7da70312ebb5be17a3eb5b
Marco Guidi <mark.88g@gmail.com>
2020-12-03 10:52:34 +0100

    stilata sezione news

--- assets/main.js
+++ assets/main.js
@@ -77,10 +77,15 @@ var app = new Vue({
         ], // all dishes: array of dish-obj
         news: [
             {
-                title: "titolo", // title string
+                title: "the pancake burger", // title string
                 article: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", // article string
-                date: "day",
-            }, // single-news-obj
+                img: "assets/img/news/pancake-burger-800x600.jpg",
+            }, // the pancake burger
+            {
+                title: "new milkshake menu", // title string
+                article: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", // article string
+                img: "assets/img/news/new-milkshake-menu-800x600.jpg",
+            }, // new milk shake menu
         ], // all news: array of new-obj
         navMenu: {
             "navigate": {
